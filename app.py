c187c7e5d8aaf05aecf707e777cb2415dd06534d
kimdoyoung <gozld1209@playwith.co.kr>
2025-07-18 18:52:59 +0900

    Fix host and prt for Render

--- app.py
+++ app.py
@@ -11,4 +11,5 @@ def slash_command():
     })
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
