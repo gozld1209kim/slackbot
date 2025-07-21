09398092edada8f3d65adc9b85d396254f0da2f8
kimdoyoung <gozld1209@playwith.co.kr>
2025-07-21 10:25:46 +0900

    updata solo msg

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 from flask import Flask, request, jsonify
-import os
 
 app = Flask(__name__)
 
 @app.route("/slack/command", methods=["POST"])
 def slash_command():
-    # 하이퍼링크 적용: <URL|텍스트>
-    response_text = "🧙 치트키 바로가기 👉 <https://docs.google.com/spreadsheets/d/1wR7HfkOxMP8xeWPNuhTQXGN9cdFFgWTEWrUp_1MBXSQ/edit?gid=948875907|여기를 클릭하세요>"
+    response_text = "🧙 [치트키 링크](https://docs.google.com/spreadsheets/d/1wR7HfkOxMP8xeWPNuhTQXGN9cdFFgWTEWrUp_1MBXSQ/edit?gid=948875907)"
     return jsonify({
-        "response_type": "in_channel",
+        "response_type": "ephemeral",  # ← 요거만 바꾸면 됨!
         "text": response_text
     })
 
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
