893eb115c18b115bf967003cea40b60d17074c59
kimdoyoung <gozld1209@playwith.co.kr>
2025-07-21 10:12:17 +0900

    updata link

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from flask import Flask, request, jsonify
-import os  # <- 추가해야 합니다
+import os
 
 app = Flask(__name__)
 
 @app.route("/slack/command", methods=["POST"])
 def slash_command():
-    response_text = "🧙 치트키 바로가기 👉 https://your-link.com/cheat"
+    # 하이퍼링크 적용: <URL|텍스트>
+    response_text = "🧙 치트키 바로가기 👉 <https://docs.google.com/spreadsheets/d/1wR7HfkOxMP8xeWPNuhTQXGN9cdFFgWTEWrUp_1MBXSQ/edit?gid=948875907|여기를 클릭하세요>"
     return jsonify({
         "response_type": "in_channel",
         "text": response_text
     })
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render가 설정한 포트를 사용
-    app.run(host="0.0.0.0", port=port)  # 외부에서 접근 가능하게 설정
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
